a70dfee73b6019c948513e2df12ac6f03623d310
Harsh <harshjaiswal28555@gmail.com>
2023-10-19 01:40:09 +0530

    routes for get post method

---
+++ models/model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const dataSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  age: {
+    required: true,
+    type: Number,
+  },
+});
+module.exports = mongoose.model("Data", dataSchema);
