47279e9b0445a0cd9e5b2759b742e2e4f4872382
Bhavish <bhavish.muneshwar@rasonix.com>
2023-10-22 15:24:43 +0530

    mail setup added

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,46 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+
+async function SendMail(user_name, user_email) {
+  console.log("email........", user_email);
+  let testAccount = await nodemailer.createTestAccount();
+  let transporter = await nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "chatapp585@gmail.com",
+      pass: "sfmfgzccqbafvtmj",
+    },
+  });
+
+  let info = {
+    from: '"chatapp585@gmail.com"',
+    to: `${user_email}`,
+    subject: ` Hello,Mr/Mrs : ${user_name}, /n Your Chat App Verification Code`,
+    text: `Hello,Mr/Mrs : ${user_name}, We Are Happy Have You On Board 👌😀`,
+    html: `<body style="font-family: Arial, sans-serif; text-align: center; background-color: #f4f4f4; padding: 20px;">
+      <div style="background-color: #ffffff; max-width: 600px; margin: auto; border-radius: 10px; padding: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+        <h1 style="color: #333;">Email Verification</h1>
+        <p style="color: #555; font-size: 16px;">Dear ${user_name},</p>
+        <p style="color: #555; font-size: 16px;">Thank you for registering with our platform. To get started, please click the link below to verify your email address:</p>
+        <a href="{verificationLink}" style="display: inline-block; text-decoration: none; background-color: #007bff; color: #ffffff; padding: 10px 20px; font-size: 16px; border-radius: 5px; margin-top: 20px;">Verify Email</a>
+        <p style="color: #555; font-size: 16px; margin-top: 20px;">If the button above doesn't work, you can also copy and paste the following link into your browser:</p>
+        <p style="color: #555; font-size: 16px;"></p>
+        <p style="color: #555; font-size: 16px;">Thank you for using our platform!</p>
+      </div>
+    </body>`,
+  };
+
+  transporter.sendMail(info, (err) => {
+    if (err) {
+      return;
+    } else {
+      return;
+    }
+  });
+}
+
 router.get("/gitHubJson", (req, res) => {
   res.json([
     {
@@ -62,9 +102,12 @@ router.post("/register", async (req, res) => {
       mobileNumber: req.body.mobileNumber,
       password: bcrypt.hashSync(req.body.password, 10),
     });
+    SendMail(req.body.name, req.body.email);
     user = await user.save();
     if (!user) {
-      return res.status(404).json({success:false,message:"The user cannot be created"});
+      return res
+        .status(404)
+        .json({ success: false, message: "The user cannot be created" });
     }
     res.status(200).json({ success: true, Data: user });
   } catch (error) {
@@ -75,7 +118,9 @@ router.post("/login", async (req, res) => {
   try {
     const userByEmail = await User.findOne({ email: req.body.email });
     if (!userByEmail) {
-      return res.status(400).json({success:false,message:"Invalid Email or Mobile Number"});
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Email or Mobile Number" });
     }
     if (
       userByEmail &&
@@ -91,9 +136,8 @@ router.post("/login", async (req, res) => {
       return res
         .status(200)
         .json({ success: true, Message: "Login Successful", token });
-    }
-    else{
-        res.status(405).json({success:false,message:"Invaid Password"})
+    } else {
+      res.status(405).json({ success: false, message: "Invaid Password" });
     }
   } catch (err) {
     console.log(err);
@@ -110,5 +154,35 @@ router.patch("/update:id", (req, res) => {
 });
 router.delete("/delete:id", (req, res) => {
   res.send("Delete by Id API");
+});
+
+router.get("/otp", async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = await nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "chatapp585@gmail.com",
+      pass: "sfmfgzccqbafvtmj",
+    },
+  });
+
+  let info = {
+    from: '"chatapp585@gmail.com"',
+    to: "bhavishm009@gmail.com",
+    subject: "Message",
+    text: "I hope this message gets delivered!",
+    html: "<b>Hello world?</b>",
+  };
+
+  transporter.sendMail(info, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+  console.log("Message sent: %s", info.messageId);
+  res.json(info);
 });
 module.exports = router;
